0baed01ca076f186b7787f95a1b240cfb84f1fb2
steve bendiola <steve.bendiola@gmail.com>
2023-12-20 09:36:51 -0600

    rename

--- 2023/rust/d-01/src/main.rs
+++ 2023/rust/d-01/src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use std::process::Command;
+
+pub(crate) fn main() {
+    ["part_01", "part_02"].iter().for_each(|bin| {
+        let output = Command::new("cargo")
+            .args(&["run", "--package", "d-01", "--bin", bin])
+            .output();
+
+        match output {
+            Ok(output) => {
+                if output.status.success() {
+                    println!("Output: {}", String::from_utf8_lossy(&output.stdout));
+                } else {
+                    println!("Error: {}", String::from_utf8_lossy(&output.stderr));
+                }
+            }
+            Err(e) => {
+                println!("Error: {}", e);
+            }
+        }
+    });
 }
