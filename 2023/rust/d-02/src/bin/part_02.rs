55ce2a37ed0e773ca3c45ffd92ef34606fb78d4b
steve bendiola <steve.bendiola@gmail.com>
2023-12-20 12:33:14 -0600

    day2 part2

---
+++ 2023/rust/d-02/src/bin/part_02.rs
@@ -0,0 +1,304 @@
+#[derive(Debug, Clone, Eq, PartialEq)]
+enum Cube {
+    Red,
+    Green,
+    Blue,
+}
+
+#[derive(Debug, Clone, Eq, PartialEq, Default)]
+struct Drawing {
+    red: usize,
+    green: usize,
+    blue: usize,
+}
+
+#[derive(Debug, Clone, Eq, PartialEq)]
+struct Game {
+    id: usize,
+    draws: Vec<Drawing>,
+}
+
+#[derive(Debug, Clone, Eq, PartialEq, Default)]
+struct Settings {
+    red: usize,
+    green: usize,
+    blue: usize,
+}
+use regex::Regex;
+
+fn game_from_text(line: &str) -> Game {
+    let mut draws = Vec::new();
+
+    let draw_text = Regex::new(r"^Game (\d+): (.*)")
+        .expect("failed to get draw regex")
+        .captures(line)
+        .expect("failed to find draw");
+    let game_id = draw_text
+        .get(1)
+        .expect("failed to extract group id")
+        .as_str()
+        .parse::<usize>()
+        .expect(format!("failed to parse id {:?}", line).as_str());
+
+    draw_text
+        .get(2)
+        .expect("failed to extract group draw")
+        .as_str()
+        .split(";")
+        .for_each(|draw_line| {
+            let counts =
+                draw_line
+                    .split(",")
+                    .fold(Settings::default(), |mut acc, mut color_count| {
+                        color_count = color_count.trim();
+                        let color_count_match = Regex::new(r"^(\d+) (red|blue|green)")
+                            .expect("failed to get color counts")
+                            .captures(color_count)
+                            .expect(
+                                format!("failed to extract color counts {:?}", color_count)
+                                    .as_str(),
+                            );
+
+                        let count = color_count_match
+                            .get(1)
+                            .expect("failed to get count")
+                            .as_str()
+                            .trim()
+                            .parse::<usize>()
+                            .expect("failed to parse count");
+
+                        let color_text = color_count_match
+                            .get(2)
+                            .expect(format!("failed to get color {:?}", color_count).as_str())
+                            .as_str()
+                            .trim();
+
+                        match color_text {
+                            "red" => acc.red += count,
+                            "blue" => acc.blue += count,
+                            "green" => acc.green += count,
+                            _ => panic!("unknown color"),
+                        }
+                        acc
+                    });
+
+            draws.push(Drawing {
+                red: counts.red,
+                green: counts.green,
+                blue: counts.blue,
+            });
+        });
+
+    Game {
+        id: game_id,
+        draws: draws,
+    }
+}
+
+use std::fs;
+pub fn main() {
+    const params: Settings = Settings {
+        red: 12,
+        green: 13,
+        blue: 14,
+    };
+    let initial: usize = 0;
+    let power_count = fs::read_to_string("resources/input")
+        .expect("failed opening file")
+        .lines()
+        .fold(initial, |acc, line| {
+            let g = game_from_text(line);
+            let id = g.id;
+            let power = power(g);
+            println!("line:{} power:{}", line, power);
+            acc + power
+        });
+
+    println!("day2 part 2 power_count:{:?}", power_count);
+}
+
+fn power(game: Game) -> usize {
+    let result = game
+        .draws
+        .iter()
+        .fold(Settings::default(), |mut acc, draw| {
+            acc.red = std::cmp::max(acc.red, draw.red);
+            acc.blue = std::cmp::max(acc.blue, draw.blue);
+            acc.green = std::cmp::max(acc.green, draw.green);
+            acc
+        });
+    let pow = result.red * result.blue * result.green;
+    print!(
+        "{} r:{} g:{} b:{} result:{:?}",
+        game.id, result.red, result.green, result.blue, pow
+    );
+
+    pow
+}
+
+#[cfg(test)]
+extern crate quickcheck;
+
+#[cfg(test)]
+#[macro_use(quickcheck)]
+extern crate quickcheck_macros;
+
+use std::fmt::Debug;
+
+#[cfg(test)]
+use quickcheck::{Arbitrary, Gen};
+
+#[cfg(test)]
+impl Arbitrary for Game {
+    fn arbitrary(g: &mut Gen) -> Self {
+        Game {
+            id: usize::arbitrary(g),
+            draws: Vec::arbitrary(g),
+        }
+    }
+}
+#[cfg(test)]
+impl Arbitrary for Drawing {
+    fn arbitrary(g: &mut Gen) -> Self {
+        let red = usize::arbitrary(g);
+        let green = usize::arbitrary(g);
+        let blue = usize::arbitrary(g);
+        Drawing { red, green, blue }
+    }
+}
+#[cfg(test)]
+impl Arbitrary for Settings {
+    fn arbitrary(g: &mut Gen) -> Self {
+        let red = usize::arbitrary(g);
+        let green = usize::arbitrary(g);
+        let blue = usize::arbitrary(g);
+        Settings { red, green, blue }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_game_from_text() {
+        let expected_values: HashMap<&str, Game> = [
+            (
+                ("Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green"),
+                Game {
+                    id: 1,
+                    draws: vec![
+                        Drawing {
+                            red: 4,
+                            green: 0,
+                            blue: 3,
+                        },
+                        Drawing {
+                            red: 1,
+                            green: 2,
+                            blue: 6,
+                        },
+                        Drawing {
+                            red: 0,
+                            green: 2,
+                            blue: 0,
+                        },
+                    ],
+                },
+            ),
+            (
+                "Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue",
+                Game {
+                    id: 2,
+                    draws: vec![
+                        Drawing {
+                            red: 0,
+                            green: 2,
+                            blue: 1,
+                        },
+                        Drawing {
+                            red: 1,
+                            green: 3,
+                            blue: 4,
+                        },
+                        Drawing {
+                            red: 0,
+                            green: 1,
+                            blue: 1,
+                        },
+                    ],
+                },
+            ),
+            (
+                "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red",
+                Game {
+                    id: 3,
+                    draws: vec![
+                        Drawing {
+                            red: 20,
+                            green: 8,
+                            blue: 6,
+                        },
+                        Drawing {
+                            red: 4,
+                            green: 13,
+                            blue: 5,
+                        },
+                        Drawing {
+                            red: 1,
+                            green: 5,
+                            blue: 0,
+                        },
+                    ],
+                },
+            ),
+            (
+                "Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red",
+                Game {
+                    id: 4,
+                    draws: vec![
+                        Drawing {
+                            red: 3,
+                            green: 1,
+                            blue: 6,
+                        },
+                        Drawing {
+                            red: 6,
+                            green: 3,
+                            blue: 0,
+                        },
+                        Drawing {
+                            red: 14,
+                            green: 3,
+                            blue: 15,
+                        },
+                    ],
+                },
+            ),
+            (
+                "Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green",
+                Game {
+                    id: 5,
+                    draws: vec![
+                        Drawing {
+                            red: 6,
+                            green: 3,
+                            blue: 1,
+                        },
+                        Drawing {
+                            red: 1,
+                            green: 2,
+                            blue: 2,
+                        },
+                    ],
+                },
+            ),
+        ]
+        .iter()
+        .cloned()
+        .collect();
+
+        expected_values.iter().for_each(|(text, game_expected)| {
+            let game_actual = game_from_text(text);
+            assert_eq!(game_actual, game_expected.clone());
+        });
+    }
+}
